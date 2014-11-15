115aeb75277ae09ac9b12fa43071cc868ffdeded
osense <krupicka.adam@gmail.com>
2014-11-15 12:23:34 +0100

    Rewrote server_sup:get_server(name, Name) to look better.

--- src/server_sup.erl
+++ src/server_sup.erl
@@ -18,8 +18,7 @@ start_link() ->
 
 start_server(Serv, Port) ->
     log:log("starting server " ++ Serv ++ " on port " ++ integer_to_list(Port)),
-    {ok, Pid} = supervisor:start_child(?MODULE, [Serv, Port]),
-    Pid.
+    supervisor:start_child(?MODULE, [Serv, Port]).
 
 kill_server(Name) ->
     ok = supervisor:terminate_child(?MODULE, get_server(name, Name)),
@@ -31,7 +30,15 @@ get_server(index, Index) ->
     ChildPid;
 get_server(name, Name) ->
     ServPids = lists:map(fun({_, Child, _, _}) -> Child end, supervisor:which_children(?MODULE)),
-    find_server_name(ServPids, Name).
+    FindServFun = fun F([H | Tail], N) ->
+        case server:get_name(H) of
+            Name ->
+                H;
+            _ ->
+                F(Tail, N)
+        end
+    end,
+    FindServFun(ServPids, Name).
 
 
 %% ===================================================================
@@ -42,18 +49,3 @@ init([]) ->
     {ok, {{simple_one_for_one, 1, 5},
           [{server, {server, start_link, []},
             transient, 5000, worker, [server]}]}}.
-
-
-%% ===================================================================
-%% private functions
-%% ===================================================================
-
-find_server_name([], _) ->
-    error;
-find_server_name([H | Tail], Name) ->
-    case server:get_name(H) of
-        Name ->
-            H;
-        _ ->
-            find_server_name(Tail, Name)
-    end.
